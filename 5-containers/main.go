9e59056c1afb5ddf1e52c389d5803c4a282c3144
Rob Kenis <rob.kenis@nike.com>
2021-08-23 15:10:35 +0200

    Modify main.go to log insult on every request

--- 5-containers/main.go
+++ 5-containers/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -21,12 +23,24 @@ func getenv(key, fallback string) string {
 }
 
 func up(w http.ResponseWriter, req *http.Request) {
+	log.Printf("Requesting insult")
+	resp, err := http.Get("https://evilinsult.com/generate_insult.php?lang=en")
+	if err != nil {
+		// handle error
+		log.Printf("Failed to request insult")
+		log.Printf(err.Error())
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	log.Printf(string(body))
 	bytes, _ := json.Marshal(Up{Message: "UP"})
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintf(w, string(bytes))
 }
 
 func main() {
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
 	var port = getenv("PORT", "8090")
 	log.Printf("Server started on port %s", port)
 
