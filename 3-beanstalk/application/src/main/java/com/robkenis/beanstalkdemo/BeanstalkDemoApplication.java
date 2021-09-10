5eb3e012b4d43d1ceb3b69b4432b6a33421c3d10
Rob Kenis <rob.kenis@nike.com>
2021-09-10 08:52:56 +0200

    Add everything on Kubernetes

--- 3-beanstalk/application/src/main/java/com/robkenis/beanstalkdemo/BeanstalkDemoApplication.java
+++ 3-beanstalk/application/src/main/java/com/robkenis/beanstalkdemo/BeanstalkDemoApplication.java
@@ -1,13 +1,36 @@
 package com.robkenis.beanstalkdemo;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
 
 @SpringBootApplication
+@RestController
 public class BeanstalkDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeanstalkDemoApplication.class, args);
+	}
+
+	private final AmazonDynamoDB client = AmazonDynamoDBClient.builder().withRegion(Regions.EU_WEST_1).build();
+
+	@PostMapping
+	public void save(){
+		client.putItem("table-van-rob", Map.ofEntries(
+				Map.entry("id", new AttributeValue().withS(UUID.randomUUID().toString())),
+				Map.entry("timestamp", new AttributeValue().withS(new Date().toString()))
+		));
 	}
 
 }
---
+++ 999-kubernetes/README.md
@@ -0,0 +1,35 @@
+# Learning Kubernetes
+https://kubernetes.io/docs/reference/kubectl/cheatsheet/
+
+## Setting up a cluster
+Follow the instructions in the [installation guide](https://minikube.sigs.k8s.io/docs/start/) to install Minikube on 
+your local machine. After installing, start the cluster using `minikube start`
+
+## Deploying an application
+```shell
+kubectl create deployment --image nginx nginx
+```
+
+## Making this reproducible
+```shell
+kubectl get deployment nginx -o yaml
+```
+This outputs a lot of yaml that is used to describe resources on Kubernetes.
+```shell
+kubectl apply -f deployment.yaml
+```
+
+## Exposing the application
+```shell
+kubectl expose deployment nginx --port=80 --type=NodePort
+```
+And access it from your local machine
+```shell
+minikube service --url nginx
+```
+
+
+# Deploying on AWS EKS
+The `deployment.yaml` and `service.yaml` can be reused on EKS. To expose the application to the internet
+create an ingress. The ingress controller installed on the cluster is the [AWS ALB Controller](https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.2/). Make sure the ingress is 
+public-facing with a nice DNS record.
